2060f693932672b245bc2fd273ee4fe3b47f6363
PorterJPayne <porterpayne04@gmail.com>
2026-07-22 11:27:57 -0600

    f

--- weather.py
+++ weather.py
@@ -1,1 +1,13 @@
+import os
+import resend
 
+resend.api_key = os.environ["RESEND_API_KEY"]
+
+response = resend.Emails.send({
+    "from": "onboarding@resend.dev",
+    "to": "porterpayne04@gmail.com",
+    "subject": "My first GitHub email",
+    "html": "<h1>🎉 It worked!</h1><p>This email was sent using GitHub Actions and Resend.</p>"
+})
+
+print(response)
